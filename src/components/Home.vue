26bc44a851d08f5a2ece469351ad86f19dbb719e
Luiz Lelis <luizhlelis@gmail.com>
2021-11-02 01:32:56 -0300

    fix: warnings in console closes #3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -124,8 +124,7 @@
       <v-card>
         <youtube
           :video-id="videoId"
-          @ready="ready"
-          @playing="playing"
+          :host="'https://youtube.com'"
         ></youtube>
       </v-card>
     </v-dialog>
@@ -164,32 +163,29 @@ export default {
     };
   },
   watch: {
-    dialog(value) {
-      if (!value) {
-        this.pause();
-      }
-    },
+    // dialog(value) {
+    //   if (!value) {
+    //     this.pause();
+    //   }
+    // },
   },
   methods: {
     // ready(event) {
     //   this.player = event.target;
     // },
-    // playing(event) {
-    //   // The player is playing a video.
+    // change() {
+    //   // when you change the value, the player will also change.
+    //   // If you would like to change `playerVars`, please change it before you change `videoId`.
+    //   // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
+    //   // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
+    //   this.videoId = "another video id";
     // },
-    change() {
-      // when you change the value, the player will also change.
-      // If you would like to change `playerVars`, please change it before you change `videoId`.
-      // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
-      // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
-      this.videoId = "another video id";
-    },
-    stop() {
-      this.player.stopVideo();
-    },
-    pause() {
-      this.player.pauseVideo();
-    },
+    // stop() {
+    //   this.player.stopVideo();
+    // },
+    // pause() {
+    //   this.player.pauseVideo();
+    // },
   },
 };
 </script>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -5,27 +5,47 @@
     </v-col>
     <v-col style="min-height: 50px; min-width:100px; text-align: center; margin-top: 11px;">
       <v-row style="vertical-align: text-bottom;">
-        <logo-text class="accent--text">Lelis</logo-text>
+        <p class="logo-text" style="margin-bottom: 0px;">Lelis</p>
       </v-row>
       <v-row style="margin-top: 0px;vertical-align: text-top;">
-        <logo-sub-text class="info--text">DEV</logo-sub-text>
+        <p class="logo-sub-text" style="margin-bottom: 0px;">DEV</p>
       </v-row>
     </v-col>
   </v-row>
 </template>
 
-<style scoped>
+<script>
+  export default {
+    name: 'Logo',
+    props: {
+      //
+    },
+    data () {
+      return {
+        //
+      }
+    },
+    computed: {
+      //
+    },
+    methods: {
+      //
+    }
+  }
+</script>
+
+<style>
   .image-logo {
     max-width: 40px;
   }
 
-  logo-text {
+  .logo-text {
     font-family: 'Bungee Inline', cursive;
     font-size: 20px;
   }
 
-  logo-sub-text {
+  .logo-sub-text {
     font-family: 'Bungee Inline', cursive;
-    font-size: 31px
+    font-size: 31px;
   } 
 </style>
