5947a2fe76c47cbbec4ae82f54870f0260f496b7
Luiz Lelis <luizhlelis@gmail.com>
2021-11-19 00:50:24 -0300

    fix: meta pckg and router disabled (not required)

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueYouTubeEmbed from 'vue-youtube-embed'
-import router from './router'
-import VueMeta from 'vue-meta'
+// import router from './router'
+// import VueMeta from 'vue-meta'
 
 Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
 
-Vue.use(VueMeta)
+// Vue.use(VueMeta)
 
 new Vue({
   vuetify,
-  router,
+  // router,
   render: h => h(App)
 }).$mount('#app')
