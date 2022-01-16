070bfe7a108cbc5296b38610beea6abb425fce70
Luiz Lelis <luizhlelis@gmail.com>
2022-01-16 00:53:59 -0300

    fix: `seo` improve 404 page responsiveness

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,9 +1,9 @@
 <template>
-  <center>
-    <h1>Not Found!</h1>
+  <center style="margin: auto; width: -webkit-fill-available; max-width: 800px;">
+    <h1 :class="fontSizeH1">Not Found!</h1>
     <p>
       <a style="color: rgb(135, 142, 136)" href="/">
-        <h3>Bring me back to home</h3>
+        <h2 :class="fontSizeH2">Bring me back to home</h2>
       </a>
     </p>
   <img alt="404" src="https://illustatus.herokuapp.com/?title=&fill=%234f86ed"/>
@@ -13,7 +13,30 @@
 <script>
 
   export default {
-    name: 'NotFound'
+    name: 'NotFound',
+    computed: {
+      fontSizeH1 () {
+        console.log(this.$vuetify.theme);
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': return 'font-h1-xs'
+          case 'sm': return 'font-h1-sm'
+          case 'md': return 'font-h1-md'
+          case 'lg': return 'font-h1-lg'
+          case 'xl': return 'font-h1-xl'
+        }
+        return ''
+      },
+      fontSizeH2 () {
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': return 'font-h2-xs'
+          case 'sm': return 'font-h2-sm'
+          case 'md': return 'font-h2-md'
+          case 'lg': return 'font-h2-lg'
+          case 'xl': return 'font-h2-xl'
+        }
+        return ''
+      },
+    }
   }
 
 </script>
