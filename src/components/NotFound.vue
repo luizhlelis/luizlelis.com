41589ee5ef70ab841774e0b32c4a797421f8eea1
Luiz Lelis <luizhlelis@gmail.com>
2022-01-15 23:47:46 -0300

    chore: `seo` create a static 404/NotFounud page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -2,8 +2,8 @@
   <center>
     <h1>Not Found!</h1>
     <p>
-      <a href="/">
-        <h3 style="color: rgb(135, 142, 136)">Go to home page</h3>
+      <a style="color: rgb(135, 142, 136)" href="/">
+        <h3>Bring me back to home</h3>
       </a>
     </p>
   <img alt="404" src="https://illustatus.herokuapp.com/?title=&fill=%234f86ed"/>
