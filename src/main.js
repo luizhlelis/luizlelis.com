e84610a19867cf3291706b4340f5b0ff0d075ca8
Luiz Lelis <luizhlelis@gmail.com>
2022-02-06 21:03:32 -0300

    chore: new routes based on articles already published

--- src/main.js
+++ src/main.js
@@ -21,9 +21,28 @@ const router = new Router({
       component: Home
     },
     {
-      path: '/article',
-      name: 'article',
-      component: Article
+      path: '/blog/dotnet-docker-images-deleted',
+      name: 'dotnet-docker-images-deleted',
+      component: Article,
+      props: { articleId: 'dotnet-docker-images-deleted', imgRef: 'https://res.cloudinary.com/practicaldev/image/fetch/s--SEo8hwIp--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5lpakyzl6myzgo4eoiby.png' }
+    },
+    {
+      path: '/blog/tracecontext',
+      name: 'tracecontext',
+      component: Article,
+      props: { articleId: 'tracecontext', imgRef: 'https://raw.githubusercontent.com/luizhlelis/trace-context-w3c/main/doc/trace-context-cover-image.png' }
+    },
+    {
+      path: '/blog/tracecontext-dotnet',
+      name: 'tracecontext-dotnet',
+      component: Article,
+      props: { articleId: 'tracecontext-dotnet', imgRef: 'https://raw.githubusercontent.com/luizhlelis/dotnet-trace-context/main/doc/trace-context-cover-image.png' }
+    },
+    {
+      path: '/blog/typesense',
+      name: 'typesense',
+      component: Article,
+      props: { articleId: 'typesense', imgRef: 'https://raw.githubusercontent.com/luizhlelis/typesense-playground/main/doc/article-hero.png' }
     },
     {
       path: '*',
